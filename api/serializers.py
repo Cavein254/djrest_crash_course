18fba034ef33a527fdff71c5a0988d6607fe624d
johnmuthua <caveomuthama@gmail.com>
2025-06-19 17:56:34 +0300

    feat: add orders

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = (
+            'id',
             'name',
             'description',
             'price',
@@ -17,3 +18,15 @@ class ProductSerializer(serializers.ModelSerializer):
                 "Price must be greater than 0"
             )
         return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ('product', 'quantity')
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ('order_id', 'created_at', 'user', 'status', 'items')
