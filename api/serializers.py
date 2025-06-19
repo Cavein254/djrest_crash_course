f6956f69ea5d9dd64184c65d6b9521c321237520
johnmuthua <caveomuthama@gmail.com>
2025-06-19 18:59:19 +0300

    feat: add class ProductInfo

--- api/serializers.py
+++ api/serializers.py
@@ -21,9 +21,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price', decimal_places=2, max_digits=10)
+
     class Meta:
         model = OrderItem
-        fields = ('product', 'quantity')
+        fields = ('product_name','product_price', 'quantity', 'item_subtotal')
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
@@ -35,3 +38,9 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('order_id', 'created_at', 'user', 'status', 'items', 'total_price')
+
+
+class ProductInfoSerializer (serializers.Serializer):
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
